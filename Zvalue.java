0682b53ce00f5be2c5cd920862cd2b0567ec9644
Nic-55 <126500735+Nic-55@users.noreply.github.com>
2024-04-04 21:57:24 -0500

    Add files via upload

---
+++ Zvalue.java
@@ -0,0 +1,14 @@
+
+public class Zvalue {
+	public static void main(String[]args)
+	{
+		//find out the value of z
+		int x,y,z;
+		x = 5;
+		y = x + 2;
+		x = 10;
+		z = y * x;
+		System.out.print("Value of z is " + z);
+	}
+
+}
