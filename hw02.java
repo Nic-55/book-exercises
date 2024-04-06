19732145d294dd4502aaf36b04ab11e62c14ffd2
Nic-55 <126500735+Nic-55@users.noreply.github.com>
2024-04-06 12:37:42 -0500

    Add files via upload

---
+++ hw02.java
@@ -0,0 +1,7 @@
+//将个人基本信息(姓名、性别、籍贯、住址)打印到控制台上输出.各条信息分别占一行
+public class hw02 {
+	public static void main(String[]args){
+		System.out.println("姓名\t性别\t籍贯\t住址\nNicole\tFemale\tMN\tKevin Lane 123");
+	}
+
+}
