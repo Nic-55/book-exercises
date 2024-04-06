430a2579e5917d2059274be7170679b02dd24955
Nic-55 <126500735+Nic-55@users.noreply.github.com>
2024-04-06 12:37:58 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,14 @@
+//java intro快速入门，开发步骤
+//对本段代码的相关说明
+//1. 下行的public class Hello 表示Hello是一个类（class)，一个公有的class
+//2. Hello {}表示一个class的开始和结束
+//3. public static void main(String[]args) 表示一个主方法（main），即我们程序的入口
+//4. main后面跟着的{}中括号，表示方法的开始和结束
+//5. System.out.println("hello,world.")；这句，表示输出括号双引号中的内容到屏幕，此处为"hello,world."
+//6. 上句结尾处的;分号，表示语句的结束。
+public class Hello {
+    //编写一个主方法main
+    public static void main(String args[]) {
+        System.out.println("老王 is learning java.");
+    }
+}
